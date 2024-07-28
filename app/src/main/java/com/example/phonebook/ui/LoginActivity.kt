d556de9c2cd682ba371f10023dec40618a7d7d2e
JhoanDev <jhoandev.log@gmail.com>
2024-07-28 19:33:25 -0300

    feat: logout

--- app/src/main/java/com/example/phonebook/ui/LoginActivity.kt
+++ app/src/main/java/com/example/phonebook/ui/LoginActivity.kt
@@ -1,25 +1,67 @@
 package com.example.phonebook.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.phonebook.database.DBHelper
 import com.example.phonebook.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonLogin.setOnClickListener{
+        val db = DBHelper(this)
+
+        sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
+        val usernameT = sharedPreferences.getString("username", "")
+        if (!usernameT.isNullOrEmpty()) {
             startActivity(Intent(this, MainActivity::class.java))
         }
-        binding.textSignUp.setOnClickListener{
+
+        binding.buttonLogin.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+            val logged = binding.checkboxLogged.isChecked
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                val loginSuccess = db.login(username, password)
+                if (loginSuccess) {
+                    if (logged){
+                        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                        editor.putString("username", username)
+                        editor.apply()
+                    }
+                    Toast.makeText(
+                        applicationContext, "Login successful!", Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(
+                        applicationContext, "Invalid username or password", Toast.LENGTH_SHORT
+                    ).show()
+                    binding.editPassword.text.clear()
+                    binding.editUsername.text.clear()
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext, "Please fill in all fields", Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        binding.textSignUp.setOnClickListener {
             startActivity(Intent(this, SingUpActivity::class.java))
         }
-        binding.textRecoverPassword.setOnClickListener{}
+        binding.textRecoverPassword.setOnClickListener {}
     }
 }
--- app/src/main/java/com/example/phonebook/ui/MainActivity.kt
+++ app/src/main/java/com/example/phonebook/ui/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.phonebook.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.phonebook.R
+import com.example.phonebook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
+        binding.buttonLogout.setOnClickListener{
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("username", "")
+            editor.apply()
+            finish()
         }
     }
 }
